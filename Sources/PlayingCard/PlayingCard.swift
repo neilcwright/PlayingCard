5473f9684caf1ff4d89720a570a50a798a45a558
Neil C Wright <surrealneil7@gmail.com>
2019-06-16 19:59:39 -0700

    update

--- Sources/PlayingCard/PlayingCard.swift
+++ Sources/PlayingCard/PlayingCard.swift
@@ -1,6 +1,12 @@
 import Foundation
 
 public struct PlayingCard {
-   let rank: Rank
-   let suit: Suit
+    
+    let rank: Rank
+    let suit: Suit
+    
+    public init(rank: Rank, suit: Suit) {
+        self.rank = rank
+        self.suit = suit
+    }
 }
