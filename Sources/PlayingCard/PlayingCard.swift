250c97305502afaa6c5291374a36a68911e0bfcb
Neil C Wright <surrealneil7@gmail.com>
2019-06-16 22:22:19 -0700

    make playing card equatable

--- Sources/PlayingCard/PlayingCard.swift
+++ Sources/PlayingCard/PlayingCard.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public struct PlayingCard {
+public struct PlayingCard: Equatable {
     
     let rank: Rank
     let suit: Suit
@@ -8,5 +8,9 @@ public struct PlayingCard {
     public init(rank: Rank, suit: Suit) {
         self.rank = rank
         self.suit = suit
+    }
+    
+    static public func ==(lhs: PlayingCard, rhs: PlayingCard) -> Bool {
+        return lhs.rank == rhs.rank && lhs.suit == rhs.suit
     }
 }
